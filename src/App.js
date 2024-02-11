cef081bd360e77e31bf021109db094b4a5346195
Jesse Otim <otimdanieljesse@gmail.com>
2024-02-11 21:19:21 +0300

    Added accordion and Refactor code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Accordion from "react-bootstrap/Accordion";
 import { useState } from "react";
 import setValidated from "react";
 import validated from "react";
@@ -76,76 +77,80 @@ function App() {
 
         <Modal show={showLogin} onHide={handleLoginClose}>
           <Modal.Header closeButton>
-            <Modal.Title style={{fontWeight:'bold', textAlign:'center'}}>Login</Modal.Title>
+            <Modal.Title style={{ fontWeight: "bold", textAlign: "center" }}>
+              Login
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             {" "}
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <Row>
-              <Form.Group md="4" controlId="validationCustom01">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  required
-                  type="text"
-                  placeholder="Email"
-                  defaultValue=""
-                />
-                <Form.Control.Feedback type="invalid">
-                  Please Enter your Email
-                </Form.Control.Feedback>
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              </Form.Group>
+                <Form.Group md="4" controlId="validationCustom01">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Email"
+                    defaultValue=""
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please Enter your Email
+                  </Form.Control.Feedback>
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
 
-              <Form.Group md="6" controlId="validationCustom03">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="text" placeholder="City" required />
-                <Form.Control.Feedback type="invalid">
-                  Please Enter your Password
-                </Form.Control.Feedback>
-              </Form.Group>
-</Row>
-              <Button type="submit" className="div">Submit</Button>
+                <Form.Group md="6" controlId="validationCustom03">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="text" placeholder="City" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please Enter your Password
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Button type="submit" className="div">
+                Submit
+              </Button>
             </Form>
           </Modal.Body>
-          
         </Modal>
 
         {/* SIGNUP */}
         <Modal show={showSignup} onHide={handleSignupClose}>
           <Modal.Header closeButton>
-            <Modal.Title style={{fontWeight:'bold', textAlign:'center'}}>Signup</Modal.Title>
+            <Modal.Title style={{ fontWeight: "bold", textAlign: "center" }}>
+              Signup
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             {" "}
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              
-                <Form.Group as={Col} md="12" controlId="validationCustom01">
-                  <Form.Label>First name</Form.Label>
-                  <Form.Control
-                    required
-                    type="text"
-                    placeholder="First name"
-                    defaultValue=""
-                  />
-                  <Form.Control.Feedback type="invalid">
+              <Form.Group as={Col} md="12" controlId="validationCustom01">
+                <Form.Label>First name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="First name"
+                  defaultValue=""
+                />
+                <Form.Control.Feedback type="invalid">
                   Please Enter your First name
                 </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="12" controlId="validationCustom02">
-                  <Form.Label>Last name</Form.Label>
-                  <Form.Control
-                    required
-                    type="text"
-                    placeholder="Last name"
-                    defaultValue=""
-                  />
-                  <Form.Control.Feedback type="invalid">
+              </Form.Group>
+              <Form.Group as={Col} md="12" controlId="validationCustom02">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Last name"
+                  defaultValue=""
+                />
+                <Form.Control.Feedback type="invalid">
                   Please Enter your Last name
                 </Form.Control.Feedback>
-                </Form.Group>
-             
+              </Form.Group>
+
               <Row className="mb-3">
-              <Form.Group as={Col} md="12" controlId="validationCustom02">
+                <Form.Group as={Col} md="12" controlId="validationCustom02">
                   <Form.Label>Email Address</Form.Label>
                   <Form.Control
                     required
@@ -154,8 +159,8 @@ function App() {
                     defaultValue=""
                   />
                   <Form.Control.Feedback type="invalid">
-                  Please Enter your Email Address
-                </Form.Control.Feedback>
+                    Please Enter your Email Address
+                  </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} md="12" controlId="validationCustom02">
                   <Form.Label>Password</Form.Label>
@@ -166,11 +171,13 @@ function App() {
                     defaultValue=""
                   />
                   <Form.Control.Feedback type="invalid">
-                  Please Enter your Password
-                </Form.Control.Feedback>
+                    Please Enter your Password
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Row>
-              <Button variant="success" type="submit">Submit</Button>
+              <Button variant="success" type="submit">
+                Submit
+              </Button>
             </Form>
           </Modal.Body>
         </Modal>
@@ -180,6 +187,40 @@ function App() {
         <p>"Unleash Your Academic Potential with PaperPal!"</p>
         <div className="circle"></div> {/* Circle */}
       </header>
+      <Container className="d-flex justify-content-center align-items-center" style={{marginTop:'100px', color:'brown'}}>
+        <h1 style={{fontSize:'50px', fontWeight:'bolder'}}>Introducing a CHEAT CODE</h1>
+      </Container>
+      <Container>
+      <Row>
+        <Col> <Accordion defaultActiveKey="0" style={{marginTop:'25px'}}>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion></Col>
+      </Row>
+    </Container>
+     
     </div>
   );
 }
