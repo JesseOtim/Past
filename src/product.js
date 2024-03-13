3ec48670b504b6644b0f4f4f75d2b3a79ad2e10a
JesseOtim <otimdanieljesse@gmail.com>
2024-03-13 22:11:19 +0300

    Refactor code

--- src/product.js
+++ src/product.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Product (){
-    return(
-<div>
-    <h1>Past paper:Paperpal</h1>
-    <p>The Past & present are your future here</p>
-<h2>Be the best version of your self</h2>
-</div>
-    )
+function Product() {
+  return (
+    <div>
+      <h1>Past paper:Paperpal</h1>
+      <p>The Past & present are your future here</p>
+      <h2>Be the best version of your self</h2>
+      <p>For the future</p>
+    </div>
+  );
 }
 
 export default Product;
